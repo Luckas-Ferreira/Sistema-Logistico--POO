4398a2c2d2e5c3dd0eb5fe0a6f3daa61c01df315
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-11-10 07:06:16 -0300

    Tela Funcionario em andamento

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        TelaFuncionario t1 = new TelaFuncionario();
+        t1.funcionarioOpcoes();
+    }
+}
---
+++ TelaFuncionario.java
@@ -0,0 +1,14 @@
+public class TelaFuncionario {
+    public void funcionarioOpcoes(){
+        System.out.println("\n\n\tTELA DO FUNCIONARIO\n");
+        System.out.println("""
+            [ 1 ] Adicionar um produto  [ 1 ]
+            [ 2 ]  Remover um produto   [ 2 ] 
+            [ 3 ] Ver todos os produtos [ 3 ]
+            [ 4 ]  Pesquisar pelo nome  [ 4 ]
+            [ 5 ] Voltar../ Tela Inicial[ 5 ]
+            [ 6 ]        SAIR           [ 6 ]
+                """);
+        return;
+    }
+}
