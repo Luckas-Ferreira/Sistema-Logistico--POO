968f73030aa54c2c9b1a7404a275f9f7e1e489a9
Douglas Alves <felipe.douglas.alves@hotmail.com>
2022-11-13 23:32:11 -0300

    Criando metodo telaInicial

--- TelaInicial.java
+++ TelaInicial.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class TelaInicial {
     
     public void telaInial(){
+        Scanner input = new Scanner(System.in);
+        int escolha = input.nextInt();
+        
         System.out.println("\n\n\tMercadinho do Thiago\n");
-        System.out.println("Você é um consumidor ou trabalha no mercadinho?");
+        System.out.println("Você é um consumidor ou é funcionario do mercadinho?");
+        System.out.println("[1]   Consumidor   [1]");
+        System.out.println("[2]  Funcionario   [2]");
+        System.out.println("[3]      SAIR      [3]");
+    
     }
 }
