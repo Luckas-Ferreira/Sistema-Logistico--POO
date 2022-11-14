a3a3a5f5eec0e9e19770b1bcca7bd0c7570bff81
Douglas Alves <felipe.douglas.alves@hotmail.com>
2022-11-14 16:54:37 -0300

    Explicação classe cliente

--- Cliente.java
+++ Cliente.java
@@ -1,7 +1,13 @@
-public class Cliente {
+public class Cliente {  
+    //Intenção da classe cliente é registrar os clientes que estão a usar o sistema do mercadinho
+    //mostrando a visão de um cliente ao entrar em uma loja, onde tem as informações que por enquanto
+    //estou jugando inicialmente como a base dessa classe, onde o cliente tem seu nome, idade e o dinheiro
+    //que tem consigo para adquirir os produtos do mercado ou apenas ver os produtos disponiveis.
+    
+    // Atributos
     private String nome;
     private int idade;
     private double dinheiro;
-    
+
 
 }
