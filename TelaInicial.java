30ff2dab74f37618d1256f490811b46dd4040f26
Douglas Alves <felipe.douglas.alves@hotmail.com>
2022-11-14 16:37:15 -0300

    Ajuste no Scanner

--- TelaInicial.java
+++ TelaInicial.java
@@ -4,23 +4,23 @@ public class TelaInicial {
     
     public void telaInial(){
 
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("\n\n\tMercadinho do Thiago\n");
-        System.out.println("    Você é um consumidor ou é funcionario do mercadinho?");
-        System.out.println("[1]   Consumidor   [1]");
-        System.out.println("[2]  Funcionario   [2]");
-        System.out.println("[3]      SAIR      [3]\n");
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("\n\n\tMercadinho do Thiago\n");
+            System.out.println("    Você é um consumidor ou é funcionario do mercadinho?");
+            System.out.println("[1]   Consumidor   [1]");
+            System.out.println("[2]  Funcionario   [2]");
+            System.out.println("[3]      SAIR      [3]\n");
 
-        System.out.print("Resposta: ");
-        int resposta = input.nextInt();
+            System.out.print("Resposta: ");
+            int resposta = input.nextInt();
 
-        if (resposta == 1){
-            System.out.println("Redirecionando para tela do consumidor");
-        } else if(resposta == 2){
-            System.out.println("Redirecionando para tela do funcionario");
-        }else if(resposta == 3){
-            System.out.println("Saindo da tela do mercadinho");
+            if (resposta == 1){
+                System.out.println("Redirecionando para tela do consumidor");
+            } else if(resposta == 2){
+                System.out.println("Redirecionando para tela do funcionario");
+            }else if(resposta == 3){
+                System.out.println("Saindo da tela do mercadinho");
+            }
         }
     }
 }
