dc867559a4ae4b83b7f8c501a695271a924679fe
Douglas Alves <felipe.douglas.alves@hotmail.com>
2022-11-13 22:55:45 -0300

    Iniciando desenvolvimento Tela inicial

--- TelaInicial.java
+++ TelaInicial.java
@@ -1,6 +1,7 @@
 public class TelaInicial {
-    public static void main(String[] args) {
-        System.out.println("tela inicial");
-        System.out.println("Tela dois testando");
+    
+    public void telaInial(){
+        System.out.println("\n\n\tMercadinho do Thiago\n");
+        System.out.println("Você é um consumidor ou trabalha no mercadinho?");
     }
 }
