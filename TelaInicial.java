e4fb423b6a252595d53eabef8bb0cd98e3f4607d
Douglas Alves <felipe.douglas.alves@hotmail.com>
2022-11-14 00:12:04 -0300

    Introdução a Tela Inicial

--- TelaInicial.java
+++ TelaInicial.java
@@ -4,7 +4,7 @@ public class TelaInicial {
     
     public void telaInial(){
         Scanner input = new Scanner(System.in);
-        int escolha = input.nextInt();
+        int resposta = input.nextInt();
         
         System.out.println("\n\n\tMercadinho do Thiago\n");
         System.out.println("Você é um consumidor ou é funcionario do mercadinho?");
@@ -12,5 +12,12 @@ public class TelaInicial {
         System.out.println("[2]  Funcionario   [2]");
         System.out.println("[3]      SAIR      [3]");
     
+        if (resposta == 1){
+            System.out.println("Redirecionando para tela do consumidor");
+        } else if(resposta == 2){
+            System.out.println("Redirecionando para tela do funcionario");
+        }else if(resposta == 3){
+            System.out.println("Saindo da tela do mercadinho");
+        }
     }
 }
