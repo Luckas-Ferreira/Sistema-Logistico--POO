3da8132d26e26875a3d26bb4798b9093bec7c0c0
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-11-14 13:06:47 -0300

    .

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        CadastrarUsuario listar = new CadastrarUsuario();
-        listar.adicionarLogin(new Funcionario());
-        listar.fazerLogin();
-
-    }
-}
