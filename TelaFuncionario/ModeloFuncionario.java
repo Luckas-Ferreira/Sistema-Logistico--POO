19e9cef27a4ad776b15ca882922383415aeaafae
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-11-15 08:04:45 -0300

    Merge branch 'feature/TelaFuncionario' into develop

---
+++ TelaFuncionario/ModeloFuncionario.java
@@ -0,0 +1,9 @@
+package TelaFuncionario;
+public class ModeloFuncionario {
+    public static void main(String[] args) {
+        TelaFuncionario telaFuncionario = new TelaFuncionario();
+        telaFuncionario.opcoes();
+        telaFuncionario.opcaoClient();
+    }
+    
+}
