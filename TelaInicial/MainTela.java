5c9f97e1a541f87810a64de698d320dc05dbd0a9
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-11-15 07:46:37 -0300

    Merge branch 'develop' into feature/TelaInicial

--- TelaInicial/MainTela.java
+++ TelaInicial/MainTela.java
@@ -1,3 +1,4 @@
+package TelaInicial;
 public class MainTela {
     public static void main(String[] args) {
         TelaInicial tela = new TelaInicial();
--- TelaInicial/TelaInicial.java
+++ TelaInicial/TelaInicial.java
@@ -1,3 +1,4 @@
+package TelaInicial;
 import java.util.Scanner;
 
 public class TelaInicial {
