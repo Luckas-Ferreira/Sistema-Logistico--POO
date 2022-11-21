65998f63202e73316dc8780fde3d6b73b9492c1e
Douglas Alves <felipe.douglas.alves@hotmail.com>
2022-11-20 22:48:39 -0300

    Primeira parte das Feutures concluidas

---
+++ src/controllers/InteracaoTelaInicial.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import java.util.Scanner;
+
+import autentication.Login;
+
+public class InteracaoTelaInicial {
+   
+    public void opcaoTelaInicial(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Resposta: ");
+        int resposta = input.nextInt();
+
+        if (resposta == 1){
+            InteraçãoTelaCliente cliente = new InteraçãoTelaCliente(null, resposta, resposta);
+            cliente.opcoes();
+            cliente.opcaoClient();
+        } else if(resposta == 2){
+            Login login = new Login();
+            Funcionario fucionario = new Funcionario();
+            login.verificar(fucionario);
+                
+        }else if(resposta == 3){
+            System.out.println("Saindo da tela do mercadinho");
+        }
+        
+        input.close();
+    }
+}
--- src/views/TelaInicial.java
+++ src/views/TelaInicial.java
@@ -1,15 +1,12 @@
 package views;
-import java.util.Scanner;
 
-import autentication.Login;
-import controllers.Funcionario;
-import controllers.InteraçãoTelaCliente;
+import controllers.InteracaoTelaInicial;
+
 
 public class TelaInicial {
     
     public void start(){
 
-        Scanner input = new Scanner(System.in); 
         System.out.println("\n\n\tMercadinho do Thiago\n");
         System.out.println("Você é um consumidor?");
         System.out.println("Você é um funcionario?");
@@ -17,22 +14,7 @@ public class TelaInicial {
         System.out.println("\t[2]  Funcionario   [2]");
         System.out.println("\t[3]      SAIR      [3]\n");
 
-        System.out.print("Resposta: ");
-        int resposta = input.nextInt();
-
-        if (resposta == 1){
-            InteraçãoTelaCliente cliente = new InteraçãoTelaCliente(null, resposta, resposta);
-            cliente.opcoes();
-            cliente.opcaoClient();
-        } else if(resposta == 2){
-            Login login = new Login();
-            Funcionario fucionario = new Funcionario();
-            login.verificar(fucionario);
-                
-        }else if(resposta == 3){
-            System.out.println("Saindo da tela do mercadinho");
-        }
-        
-        input.close();
+        InteracaoTelaInicial tela = new InteracaoTelaInicial();
+        tela.opcaoTelaInicial();
     }
 }
