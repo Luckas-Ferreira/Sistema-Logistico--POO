4c05e045ec33b2e688d7fd3ee4094398cea9134d
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-11-22 15:17:17 -0300

    remorção de produtos completo

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Produtos produto = new Produtos();
-        produto.ProdutosCadastrados();
-        produto.removerProduto(new RemoverProduto("Macarrão"));
-        produto.VerProdutos();
-    }
-}
