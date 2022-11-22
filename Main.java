1c50da6d62b0c5ab9aca1ed54b410c43c6cfb412
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-11-22 15:19:16 -0300

    cadastro de produtos completo

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Produtos produto = new Produtos();
-        produto.ProdutosCadastrados();
-        produto.CadastrarNovoProduto(new Produto("Bosta", 10f, 222));
-        produto.VerProdutos();
-    }
-}
