65263d36a5747959e58a199c3985faa33afc25fb
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-11-22 15:17:57 -0300

    Compra de produtos completo

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Produtos produtos = new Produtos();
-        produtos.ProdutosCadastrados();
-        produtos.VerProdutos();
-        produtos.comprarProduto(new ComprarProduto("Caviar fds", 10));
-        produtos.VerProdutos();
-    }
-}
