d8dfd186bb9ed2e4d95531a3945d4917131fc121
Luckas-Ferreira <luckas0ferreira321@gmail.com>
2022-11-22 15:15:01 -0300

    Sistema de compra completo

--- Produto.java
+++ Produto.java
@@ -1,10 +1,12 @@
 public class Produto {
     protected String nome;
     protected float valor;
+    protected int quantidade;
     
-    public Produto(String nome, float valor){
+    public Produto(String nome, float valor, int quantidade){
         this.nome = nome;
         this.valor = valor;
+        this.quantidade = quantidade;
     }
 
     public String getNome() {
@@ -16,4 +18,15 @@ public class Produto {
         return valor;
     }
 
+    public int getQuantidade(){
+        return quantidade;
+    }
+    public void mudarQuantidade(int quantidade){
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto: " + nome + ", valor: " + valor + ", quantidade: " + quantidade;
+    }
 }
--- Produtos.java
+++ Produtos.java
@@ -2,26 +2,27 @@ import java.util.ArrayList;
 public class Produtos {
     ArrayList<Produto> produtos = new ArrayList<>();
 
-
     public void ProdutosCadastrados(){
-        produtos.add(new Produto("Arroz", 2.5f));
-        produtos.add(new Produto("Feijão", 2.5f));
-        produtos.add(new Produto("Café", 2.5f));
-        produtos.add(new Produto("Macarrão", 2.5f));
-        produtos.add(new Produto("Cuscuz", 2.5f));
-        produtos.add(new Produto("Açuca", 2.5f));
-        produtos.add(new Produto("Caviar fds", 10.000f));
+        produtos.add(new Produto("Arroz", 2.5f, 3));
+        produtos.add(new Produto("Feijão", 2.5f, 5));
+        produtos.add(new Produto("Café", 2.5f, 7));
+        produtos.add(new Produto("Macarrão", 2.5f, 7));
+        produtos.add(new Produto("Cuscuz", 2.5f, 7));
+        produtos.add(new Produto("Açuca", 2.5f, 87));
+        produtos.add(new Produto("Caviar fds", 10.000f, 87));
     }
     
      
-    public void removerProduto(ComprarProduto comprarProduto){
+    public void comprarProduto(ComprarProduto comprarProduto){
         for(int i = 0; i < produtos.size(); i++){
         Produto produto = produtos.get(i);
 
         if(produto.getNome().equals(comprarProduto.getNome())){
-            
-            // Remove.
-            produtos.remove(produto);
+            if(produto.getQuantidade()> 0){
+                produto.mudarQuantidade(produto.getQuantidade() - comprarProduto.getQuantidade());
+            }
+            // Mandar para tela perguntando qual a forma de pagamento
+            //produtos.remove(produto);
 
             // Sai do loop.
             break;
